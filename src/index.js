f26614d068782d23807eb58643da7470aa2d4ee0
greigs <greig2@gmail.com>
2019-05-16 22:19:18 +0100

    working - simple

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-//import document from "document";
 let setAttr = true
 Math.hypot = function(x, y){ return Math.sqrt(x*x + y*y) }
-
+let infinityPoint = new Point(Infinity, Infinity)
+let tmpPoint = new Point(0,0)
 
 /* Point */
 function Point(x, y) {
@@ -9,10 +9,7 @@ function Point(x, y) {
     this.y = y;
   }
   
-  // Return a new point with the values incremented by the passed-in values.
-  Point.prototype.add = function (x, y) {
-    return new Point(this.x + x, this.y + y);
-  };
+  
   
   // Return the dist between this point and another.
   Point.prototype.dist = function (other) {
@@ -23,6 +20,8 @@ function Point(x, y) {
  function Map(height) {
     this.height = height;
     this.size = 10;
+   this.tmpx = 0
+   this.tmpy = 0
     this.grid = [
       [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
       [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
@@ -38,20 +37,22 @@ function Point(x, y) {
   }
   
   Map.prototype.isWall = function (point) {
-    const gridCoordinates = toGrid(point, this.height);
-    return this.isWithinBounds(point) && this.grid[gridCoordinates.y][gridCoordinates.x] === 1;
+    this.tmpx = Math.floor(point.x / this.height);
+    this.tmpy = Math.floor(point.y / this.height);
+    return this.isWithinBoundsxy(this.tmpx,this.tmpy) && this.grid[this.tmpy][this.tmpx] === 1;
   };
   
   Map.prototype.isWithinBounds = function (point) {
-    const gridCoordinates = toGrid(point, this.height);
-    return gridCoordinates.x >= 0 && gridCoordinates.x < this.size && gridCoordinates.y >= 0 && gridCoordinates.y < this.size;
+    this.tmpx = Math.floor(point.x / this.height);
+    this.tmpy = Math.floor(point.y / this.height);
+    return this.tmpx >= 0 && this.tmpx < this.size && this.tmpy >= 0 && this.tmpy < this.size;
+  };
+
+  Map.prototype.isWithinBoundsxy = function (x,y) {
+    return x >= 0 && x < this.size && y >= 0 && y < this.size;
   };
   
-  // Convert unit coordinates to grid coordinates. Each grid coordinate can be broken up into
-  // some number of smaller "unit" coordinates.
-  function toGrid(point, unitsPerGrid) {
-    return new Point(Math.floor(point.x / unitsPerGrid), Math.floor(point.y / unitsPerGrid));
-  }
+ 
 
 /* Radians */  
 const twoPi = 2 * Math.PI;
@@ -96,14 +97,15 @@ function Ray(map, angle, origin) {
     // Calculate the coordinates of the first intersection with a grid boundary.
     const intersectionY = Math.floor(origin.y / map.height) * map.height + (up ? -0.01 : map.height);
     const intersectionX = origin.x + (origin.y - intersectionY) / Math.tan(angle);
-    const intersection = new Point(intersectionX, intersectionY);
+    tmpPoint.x = intersectionX;
+    tmpPoint.y = intersectionY;
   
     // Calculate the change in x and y coordinates needed to iterate across grid boundaries.
     const deltaY = up ? -map.height : map.height;
     const deltaX = Math.abs(map.height / Math.tan(angle)) * (right ? 1 : -1);
   
     // Find the nearest wall and return the dist to it.
-    const wall = findWall(map, intersection, deltaX, deltaY);
+    const wall = findWall(map, tmpPoint, deltaX, deltaY);
     return wall.dist(origin);
   }
   
@@ -112,14 +114,15 @@ function Ray(map, angle, origin) {
     // Calculate the coordinates of the first intersection with a grid boundary.
     const intersectionX = Math.floor(origin.x / map.height) * map.height + (right ? map.height : -0.01);
     const intersectionY = origin.y + (origin.x - intersectionX) * Math.tan(angle);
-    const intersection = new Point(intersectionX, intersectionY);
+    tmpPoint.x = intersectionX;
+    tmpPoint.y = intersectionY;
   
     // Calculate the change in x and y coordinates needed to iterate across grid boundaries.
     const deltaX = right ? map.height : -map.height;
     const deltaY = Math.abs(map.height * Math.tan(angle)) * (up ? -1 : 1);
   
     // Find the nearest wall and return the dist to it.
-    const wall = findWall(map, intersection, deltaX, deltaY);
+    const wall = findWall(map, tmpPoint, deltaX, deltaY);
     return wall.dist(origin);
   }
   
@@ -127,16 +130,17 @@ function Ray(map, angle, origin) {
   function findWall(map, position, deltaX, deltaY) {
     // If the ray is no longer within the bounds of the map, return a point infinitely far away.
     if (!map.isWithinBounds(position)) {
-      return new Point(Infinity, Infinity);
+      return infinityPoint;
     }
   
     // If the ray has hit a wall, return its position.
     if (map.isWall(position)) {
       return position;
     }
-  
+    position.x += deltaX;
+    position.y += deltaY;
     // No wall has been encountered. Iterate to the next grid boundary and check again.
-    return findWall(map, position.add(deltaX, deltaY), deltaX, deltaY);
+    return findWall(map, position, deltaX, deltaY);
   }
   
 
@@ -171,6 +175,29 @@ Player.prototype.castRays = function (map, fov, resolution) {
   const rayAngles2 = arr.map(function(_, index){return startAngle - index * angleBetweenRays});
   // Calculate the dist from each ray to the nearest wall.
   return rayAngles2.map(function(angle,index){ return new Ray(map, angle, p.position)});
+};
+
+Player.prototype.castRays2 = function (map, fov, resolution, rays) {
+  // If the field of view is 60 degrees and the resolution is 320, there is 60 / 320 degrees
+  // between each ray.
+  const angleBetweenRays = fov / resolution;
+
+  const p = this;
+  // The player's direction is the center of the screen, and the left edge of the screen is half
+  // the field of view to the left. In our coordinate system, angles increase as we turn counter-
+  // clockwise, so we add to player's current direction.
+  const startAngle = this.direction + fov / 2;
+
+  // Generate the angle for each ray starting from the left and sweeping to the right screen edge.
+  //const rayAngles = new Array(resolution).fill(0).map((_, index) => startAngle - index * angleBetweenRays);
+  rays.forEach(function(ray, index){
+     ray.map = map
+     ray.angle = startAngle - index * angleBetweenRays
+     ray.position = p.position
+     ray.angle = normalize(ray.angle);
+     ray.dist = cast(map, ray.angle, ray.origin);
+
+  })
 };
 
 Player.prototype.turnRight = function (elapsed) {
@@ -257,13 +284,14 @@ const fov = fromDegrees(60)
 const mapHeight = 64
 const map = new Map(mapHeight)
 const player = new Player(160, 160, fromDegrees(0))
-const resolution = 30
+const resolution = 15
 const screenHeight = 300
 const screenWidth = 300
 
-let color="#FF0000"
+//let color="#FF0000"
+let frames= []
 
-var rays = player.castRays(map, fov, resolution)
+let rays = player.castRays(map, fov, resolution)
 let els = rays.map(function(_, number){return document.getElementById(number + 'r')})
 
 var dist = adjustdist(rays[0], player)
@@ -271,40 +299,64 @@ var height = Math.min(mapHeight / dist * 255, screenHeight)
 var width = screenWidth / resolution
 var top1 = ((screenHeight - height) / 2)
 var left = (0 * width)
-var adjustedColor = darken(color, dist / 460)
+//var adjustedColor = darken(color, dist / 460)
 var el1 = els[0]
-el1.width = Math.round(width)
-el1.height = Math.round(height)
-el1.x = Math.round(left)
-el1.y = Math.round(top1)
-el1.style.fill = adjustedColor
+
+rays.forEach(function(ray, number) {
   
+  dist = adjustdist(rays[number], player)
+ height = Math.min(mapHeight / dist * 255, screenHeight)
+ width = screenWidth / resolution
+ top1 = ((screenHeight - height) / 2)
+ left = (number * width)
+ //adjustedColor = darken(color, dist / 460)
+ el1 = els[number]
+  el1.width = Math.round(width)
+  //el1.height = Math.round(height)
+  el1.x = Math.round(left)
+  //el1.y = Math.round(top1)
+  //el1.style.fill = adjustedColor
+  if (setAttr){
+    //el1.setAttribute('width', Math.round(width))
+    el1.setAttribute('width', Math.round(width))
+    //el1.setAttribute('x', Math.round(left))
+    el1.setAttribute('x', Math.round(left))
+    //el1.setAttribute('fill',adjustedColor)
+  }
+
+})
+
+
 
 
 function callback(timestamp) {
   // Perform animation frame logic here
-  rays = player.castRays(map, fov, resolution)
-  player.turnLeft(10)
+ 
+  player.turnLeft(100)
+  player.castRays2(map, fov, resolution, rays)
+
+  
+  width = screenWidth / resolution
   rays.forEach(function(ray, number) {
     
     dist = adjustdist(ray, player)
     height = Math.min(mapHeight / dist * 255, screenHeight)
-    width = screenWidth / resolution
+    
     top1 = ((screenHeight - height) / 2)
-    left = (number * width)
-    adjustedColor = darken(color, dist / 460)
+    //left = (number * width)
+    //adjustedColor = darken(color, dist / 460)
     el1 = els[number]
-    el1.width = Math.round(width)
-    el1.height = Math.round(height)
-    el1.x = Math.round(left)
-    el1.y = Math.round(top1)
-    el1.style.fill = adjustedColor
+    //el1.width = Math.floor(width)
+    el1.height = Math.floor(height)
+    //el1.x = Math.floor(left)
+    el1.y = Math.floor(top1)
+    //el1.style.fill = adjustedColor
     if (setAttr){
-      el1.setAttribute('width', Math.round(width))
+      //el1.setAttribute('width', Math.round(width))
       el1.setAttribute('height', Math.round(height))
-      el1.setAttribute('x', Math.round(left))
+      //el1.setAttribute('x', Math.round(left))
       el1.setAttribute('y', Math.round(top1))
-      el1.setAttribute('fill',adjustedColor)
+      //el1.setAttribute('fill',adjustedColor)
     }
 })
   // Request next frame
